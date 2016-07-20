cc36b8ed6fb8097202b203932982acde53c076b9
Andrew Aldridge <i80and@foxquill.com>
2016-07-20 18:19:21 -0400

    Integrate client, switch to more flexible schema

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3.4
 import datetime
+import json
 import logging
 import os
 import sys
 import urllib.parse
-from typing import Callable, Union
+from typing import Callable, Dict, Union
 
 import pymongo
 import werkzeug.serving
@@ -15,6 +16,55 @@ logger = logging.getLogger(__name__)
 
 EMPTY_BMP = b'BM\x1e\x00\x00\x00\x00\x00\x00\x00\x1a\x00\x00\x00\x0c\x00' \
             b'\x00\x00\x01\x00\x01\x00\x01\x00\x18\x00\x00\x00\xff\x00'
+
+
+class Vote:
+    MAX_KEY_LEN = 25
+    MAX_PAGE_LEN = 255
+    MAX_REASON_LEN = 1024
+
+    def __init__(self, page: str, useful: bool, fields: Dict[str, Union[str, bool]]) -> None:
+        self.page = page
+        self.useful = useful
+        self.fields = fields
+
+    @classmethod
+    def parse(cls, query_string: bytes) -> 'Vote':
+        try:
+            parameters = cls.parse_qs(query_string)
+
+            page = parameters['p']
+            useful = parameters['v']
+
+            if not isinstance(page, str):
+                raise TypeError('Invalid page type')
+
+            if not isinstance(useful, bool):
+                raise TypeError('Invalid useful type')
+
+            del parameters['p']
+            del parameters['v']
+
+            return cls(page[:cls.MAX_PAGE_LEN], useful, parameters)
+        except (KeyError, ValueError, TypeError) as err:
+            logger.exception('Invalid vote request')
+            raise ValueError('Invalid vote request') from err
+
+    @classmethod
+    def parse_qs(cls, query_string: bytes) -> Dict[str, Union[str, bool]]:
+        parameters = urllib.parse.parse_qs(str(query_string, 'utf-8'))
+        parsed = {}  # type: Dict[str, Union[str, bool]]
+        for key, value in parameters.items():
+            if len(key) > cls.MAX_KEY_LEN:
+                raise ValueError('Key too long')
+
+            value = json.loads(value[0][:cls.MAX_REASON_LEN])
+            if not (isinstance(value, bool) or isinstance(value, str)):
+                raise TypeError('Invalid value type')
+
+            parsed[key] = value
+
+        return parsed
 
 
 class Connection:
@@ -31,22 +81,18 @@ class Connection:
         self.votes = self.db['votes']
         self.votes.create_index('page')
 
-    def vote(self,
-             page: str,
-             useful: bool,
-             reason: str) -> None:
+    def vote(self, vote: Vote) -> None:
         """Record a vote for the given page path."""
-        self.votes.save({'page': page,
-                         'useful': useful,
-                         'date': datetime.datetime.utcnow(),
-                         'reason': reason})
+        doc = {'page': vote.page,
+               'useful': vote.useful,
+               'date': datetime.datetime.utcnow()}
+        for key, value in vote.fields.items():
+            doc['q-{}'.format(key)] = value
+        self.votes.save(doc)
 
 
 class Deluge:
     """An HTTP frontent to Deluge."""
-    MAX_PAGE_LEN = 255
-    MAX_REASON_LEN = 1024
-
     def __init__(self, host: str) -> None:
         self.connection = Connection(host, 5*1024*1024*1024)
 
@@ -60,14 +106,11 @@ class Deluge:
             return Response('')
 
         try:
-            parameters = urllib.parse.parse_qs(str(request.query_string, 'utf-8'))
-            page = parameters['p'][0][:self.MAX_PAGE_LEN]
-            useful = bool(int(parameters['v'][0]) > 0)
-            reason = parameters.get('r', [''])[0][:self.MAX_REASON_LEN]
-        except (KeyError, ValueError):
+            vote = Vote.parse(request.query_string)
+        except ValueError:
             return BadRequest()
 
-        self.connection.vote(page, useful, reason)
+        self.connection.vote(vote)
         return Response(EMPTY_BMP, content_type='image/bmp')
 
     @classmethod
---
+++ client/src/index.js
@@ -0,0 +1,245 @@
+// @flow
+
+const FEEDBACK_URL = 'http://deluge.us-east-1.elasticbeanstalk.com/'
+
+function addQueryParameters(url: string, parameters: Map<string, string|boolean>): string {
+    const queryComponents = []
+
+    for(let [key, value] of parameters.entries()) {
+        queryComponents.push(`${encodeURIComponent(key)}=${encodeURIComponent(JSON.stringify(value))}`)
+    }
+    return url + '?' + queryComponents.join('&')
+}
+
+class PendingVote {
+    vote: boolean
+
+    constructor(vote: boolean) {
+        this.vote = vote
+    }
+}
+
+type State = PendingVote | 'Voted' | 'NotVoted'
+
+interface Question {
+    name: string;
+    clear(): void;
+    draw(): HTMLElement;
+}
+
+class FreeformQuestion {
+    name: string
+    caption: string
+    answer: ?string
+
+    constructor(name: string, caption: string) {
+        this.name = name
+        this.caption = caption
+        this.answer = null
+    }
+
+    clear(): void { this.answer = null }
+
+    draw(): HTMLElement {
+        const element = document.createElement('div')
+        const textForm = document.createElement('textarea')
+        textForm.placeholder = this.caption
+        element.appendChild(textForm)
+        element.oninput = () => { this.answer = textForm.value }
+
+        return element
+    }
+}
+
+class BinaryQuestion {
+    name: string
+    promptHtml: string
+    answer: ?boolean
+
+    constructor(name: string, promptHtml: string) {
+        this.name = name
+        this.promptHtml = promptHtml
+        this.answer = null
+    }
+
+    clear(): void { this.answer = null }
+
+    draw(): HTMLElement {
+        const element = document.createElement('div')
+        element.innerHTML = this.promptHtml
+
+        const thumbsGutter = document.createElement('div')
+        const thumbsUp = document.createElement('span')
+        const thumbsDown = document.createElement('span')
+
+        thumbsUp.className = 'switch fa fa-thumbs-up good'
+        thumbsUp.onclick = () => {
+            this.answer = true
+            thumbsUp.className = 'switch fa fa-thumbs-up good selected'
+            thumbsDown.className = 'switch fa fa-thumbs-down bad'
+        }
+
+        thumbsDown.className = 'switch fa fa-thumbs-down bad'
+        thumbsDown.onclick = () => {
+            this.answer = false
+            thumbsUp.className = 'switch fa fa-thumbs-up good'
+            thumbsDown.className = 'switch fa fa-thumbs-down bad selected'
+        }
+
+        thumbsGutter.appendChild(thumbsUp)
+        thumbsGutter.appendChild(thumbsDown)
+        element.appendChild(thumbsGutter)
+
+        return element
+    }
+}
+
+class Deluge {
+    project: string
+    path: string
+    questions: Question[]
+    state: State
+    storageKey: string
+
+    constructor(project: string, path: string) {
+        this.project = project
+        this.path = path
+        this.questions = []
+        this.state = 'NotVoted'
+
+        this.storageKey = `feedback-${project}/${path}`
+        const val = localStorage[this.storageKey]
+        const ratedDate = val? Date.parse(val).valueOf() : -Infinity;
+
+        // Expire the last rating after 30 days
+        if((new Date()).valueOf() < (ratedDate + (1000 * 60 * 60 * 24 * 30))) {
+            this.state = 'Voted'
+        }
+    }
+
+    draw(root: HTMLElement): void {
+        if(this.state === 'NotVoted') {
+            root.className = ''
+            root.innerHTML = '<p>Was this page helpful?</p>' +
+                             '<a class="button" id="rate-up">Yes</a>' +
+                             '<a class="button" id="rate-down">No</a>'
+            root.querySelector('#rate-up').onclick = () => {
+                this.state = new PendingVote(true)
+                this.draw(root)
+            }
+
+            root.querySelector('#rate-down').onclick = () => {
+                this.state = new PendingVote(false)
+                this.draw(root)
+            }
+
+            return
+        }
+
+        if(this.state === 'Voted') {
+            root.className = ''
+            root.innerHTML = '<p>Thank you for your feedback!</p>'
+            return
+        }
+
+        // Draw the survey
+        root.className = 'expanded'
+
+        // Store a copy of state to prevent changes that could result
+        // in a type error
+        const state = this.state
+
+        root.innerText = ''
+        const questionListElement = document.createElement('ul')
+
+        if(state.vote === false) {
+            const listElement = document.createElement('li')
+            listElement.innerText = 'We\'re sorry! Please help us improve this page.'
+            questionListElement.appendChild(listElement)
+        }
+
+        for(let question of this.questions) {
+            question.clear()
+            const listElement = document.createElement('li')
+            listElement.appendChild(question.draw())
+            questionListElement.appendChild(listElement)
+        }
+
+        const buttonGroup = document.createElement('div')
+        buttonGroup.className = 'button-group'
+        questionListElement.appendChild(buttonGroup)
+
+        const cancelButtonElement = document.createElement('button')
+        cancelButtonElement.innerText = 'Cancel'
+        buttonGroup.appendChild(cancelButtonElement)
+        cancelButtonElement.onclick = () => {
+            this.state = 'NotVoted'
+            this.draw(root)
+        }
+
+        const submitButtonElement = document.createElement('button')
+        submitButtonElement.innerText = 'Submit'
+        submitButtonElement.className = 'primary'
+        buttonGroup.appendChild(submitButtonElement)
+        submitButtonElement.onclick = () => {
+            const answers = new Map()
+            for(let question of this.questions) {
+                if(question.answer != null) {
+                    answers.set(question.name, question.answer)
+                }
+            }
+
+            this.sendRating(state.vote, answers).then(() => {
+                const ratedDate = (new Date()).toISOString()
+                localStorage.setItem(this.storageKey, ratedDate)
+
+                this.state = 'Voted'
+                this.draw(root)
+            }).catch(() => {
+                console.error('Failed to send feedback')
+                this.state = 'NotVoted'
+                this.draw(root)
+            })
+        }
+
+        // Finally attach our new element tree to the DOM. Delay this
+        // to prevent unnecessary browser overhead
+        root.appendChild(questionListElement)
+    }
+
+    askQuestion(name: string, html: string): Deluge {
+        const question = new BinaryQuestion(name, html)
+        this.questions.push(question)
+
+        return this
+    }
+
+    askFreeformQuestion(name: string, caption: string): Deluge {
+        const question = new FreeformQuestion(name, caption)
+        this.questions.push(question)
+
+        return this
+    }
+
+    sendRating(vote: boolean, fields: Map<string, string|boolean>): Promise<void> {
+        return new Promise((resolve, reject) => {
+            fields.set('v', vote)
+            fields.set('p', `${this.project}/${this.path}`)
+            const url = addQueryParameters(FEEDBACK_URL, fields)
+
+            // Report this rating using an image GET to work around the
+            // same-origin policy
+            const img = new Image()
+            img.onload = () => {
+
+                return resolve()
+            }
+            img.onerror = () => {
+                return reject()
+            }
+            img.src = url
+        })
+    }
+}
+
+window.Deluge = Deluge
